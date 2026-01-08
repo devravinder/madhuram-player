2d6df63f2e7b8ff1cd1ae45db0395726f3877386
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-01-08 10:52:37 +0530

    feat: scrollbar

--- src/components/layout/HeaderContent.tsx
+++ src/components/layout/HeaderContent.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "../ThemeToggle";
 export default function HeaderContent() {
   return (
     <>
-      <h1 className="w-full flex flex-row justify-between items-center">
+      <h1 className="w-full flex flex-row justify-between items-center cursor-pointer">
         <Avatar src="/favicon.svg" name="Avatar" />
         <ThemeToggle />
       </h1>
