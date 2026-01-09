2760ef11ac89a1e9116adaa8dd69841d4286b3a7
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-01-10 03:19:12 +0530

    fix: modal bg

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,7 +3,7 @@ import { type ReactNode, useState } from 'react'
 import { createPortal } from 'react-dom'
 import tw from 'tailwind-styled-components'
 
-const ModalContainer = tw.div`fixed inset-0 h-screen w-screen z-999 grid place-items-center bg-black bg-opacity-50 backdrop-blur-sm transition-opacity duration-300`
+const ModalContainer = tw.div`fixed inset-0 h-screen w-screen z-999 grid place-items-center bg-transparent backdrop-blur-xl transition-opacity duration-300`
 export default function Modal({children}:{children: ReactNode}) {
   const [ele, setElement] = useState<HTMLDivElement | null>() // like useEffect
   return (
