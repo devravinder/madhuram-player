35b138d07f2a1140cd771fbb4a9242da1d45932c
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-01-10 03:04:08 +0530

    fix: FullScreenPLayer css issues

--- src/components/Elements.tsx
+++ src/components/Elements.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@tanstack/react-router";
+import { Heart } from "lucide-react";
 import tw from "tailwind-styled-components";
 
 // Note:- don't change these layout elements (carefull)
@@ -7,19 +8,18 @@ export const Header = tw.header`glass fixed top-0 left-0 right-0 h-14 flex items
 export const Footer = tw.footer`glass fixed bottom-0 left-0 right-0 h-14 flex items-center justify-center px-4 z-10`;
 export const Main = tw.main`absolute py-14 inset-0 overflow-y-auto`; // p=14
 
+export const MainContainer = tw.section`h-full flex flex-col relative`;
+export const HeaderContainer = tw.h1`w-full flex flex-row justify-between items-center cursor-pointer`;
 
-export const MainContainer = tw.section`h-full flex flex-col relative`
-export const HeaderContainer = tw.h1`w-full flex flex-row justify-between items-center cursor-pointer`
-
-export const MainSection = tw.section`flex-1 overflow-y-auto px-4 py-3` // part of Main ( Main = MainContainer(MainSection + Player )
+export const MainSection = tw.section`flex-1 overflow-y-auto px-4 py-3`; // part of Main ( Main = MainContainer(MainSection + Player )
 
 // like main layout but for Page (route)
-export const PageLayout = tw.section`relative h-full flex flex-col gap-8 p-4`
-export const PageHeader = tw.h1``
-export const PageFooter = tw.div`fixed bottom-0 left-0 right-0 h-14`
-export const PageMain = tw.div`flex-1 overflow-y-auto`
-export const PageMainContainer = tw.div`h-full flex flex-col relative`
-export const PageMainSection = tw.div`flex-1`
+export const PageLayout = tw.section`relative h-full flex flex-col gap-8 p-4`;
+export const PageHeader = tw.h1``;
+export const PageFooter = tw.div`fixed bottom-0 left-0 right-0 h-14`;
+export const PageMain = tw.div`flex-1 overflow-y-auto`;
+export const PageMainContainer = tw.div`h-full flex flex-col relative`;
+export const PageMainSection = tw.div`flex-1`;
 
 // Elements
 
@@ -31,8 +31,15 @@ export const Uppercase = tw.span`font-semibold uppercase`;
 
 export const Card = tw.div`bg-card text-card-foreground shadow rounded-md p-4`;
 
-export const Nav=tw.nav`flex items-center justify-around py-2 w-full max-w-5xl`
+export const Nav = tw.nav`flex items-center justify-around py-2 w-full max-w-5xl`;
 
-export const NavLink = tw(Link)`text-muted-foreground flex flex-col items-center gap-1 px-4 py-2 rounded-lg transition-colors`    
+export const NavLink = tw(
+  Link
+)`text-muted-foreground flex flex-col items-center gap-1 px-4 py-2 rounded-lg transition-colors`;
 
-export const Input = tw.input`w-full bg-secondary/50 border border-border rounded-xl px-4 py-3 text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-primary/50 focus:border-primary transition-all duration-200`    
+export const Input = tw.input`w-full bg-secondary/50 border border-border rounded-xl px-4 py-3 text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-primary/50 focus:border-primary transition-all duration-200`;
+
+export const Like = tw(Heart)<{ $liked?: boolean }>`${(p) =>
+  p.$liked
+    ? "fill-red-500 text-red-500"
+    : "text-muted-foreground"} w-5 h-5 transition-all`;
--- src/components/player/FullScreenPlayer.tsx
+++ src/components/player/FullScreenPlayer.tsx
@@ -1,26 +1,31 @@
 import { usePlayer } from "@/context/PlayerContext";
 import { formatTime } from "@/services/timeUtil";
 import {
-    ChevronDown,
-    CircleEllipsis,
-    Heart,
-    Pause,
-    Play,
-    Repeat,
-    Repeat1,
-    RotateCcw,
-    RotateCw,
-    Shuffle,
-    SkipBack,
-    SkipForward,
+  CircleEllipsis,
+  ListMusic,
+  Pause,
+  Play,
+  Repeat,
+  Repeat1,
+  RotateCcw,
+  RotateCw,
+  Shuffle,
+  SkipBack,
+  SkipForward,
 } from "lucide-react";
 import { useState } from "react";
+import { Like } from "../Elements";
 import { ProgressBar } from "./ProgressBar";
 import SleeperTimer from "./SleeperTimer";
 import { VolumeSlider } from "./VolumeSlider";
+import Waves from "../Waves";
 
-export default function FullScreenPlayer({onClose}:{onClose?:VoidFunction}) {
-  const [favourite, setFavourite] = useState(false);
+export default function FullScreenPlayer({
+  onClose,
+}: {
+  onClose?: VoidFunction;
+}) {
+  const [isLiked, setIsLiked] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
 
   const {
@@ -47,152 +52,147 @@ export default function FullScreenPlayer({onClose}:{onClose?:VoidFunction}) {
   const RepeatIcon = repeatMode === "one" ? Repeat1 : Repeat;
 
   return (
-    <div className="w-full h-full flex flex-col">
-      <div className="w-full shrink-0 flex flex-row gap-2 px-4 py-4 bg-accent">
-        <div className=" grow flex flex-row justify-center items-center">
-          <span className="text-foreground">Now Playing</span>
-        </div>
-        <div className="flex flex-row justify-between items-center">
-          <button
-            className="rounded-lg p-2 cursor-pointer"
-            onClick={onClose}
-          >
-            <ChevronDown className="h-5 w-5" />
-          </button>
-        </div>
-      </div>
-      <div className="grow flex justify-center items-center">
-        <div className="h-96 w-96 p-4">
+    <div className="w-full max-w-md mx-auto h-full p-4 flex flex-col">
+      <div className="flex-1 min-h-0 flex flex-col bg-linear-to-br from-slate-900 to-slate-800 rounded-3xl shadow-2xl">
+        <div className="flex justify-center items-center relative">
           <img
             src={currentSong.coverUrl}
             alt={currentSong.title}
-            className="w-full h-full rounded-lg object-cover"
+            className="w-full aspect-square rounded-t-3xl object-cover"
           />
+          <div className="absolute inset-0 bg-linear-to-t from-slate-900/90 via-transparent to-transparent" />
+          <button
+            onClick={() => setIsLiked(!isLiked)}
+            className="absolute top-4 right-4 p-2.5 bg-black/30 backdrop-blur-sm rounded-full hover:bg-black/50 transition-all"
+          >
+            <Like $liked={isLiked} />
+          </button>
         </div>
-      </div>
-      <div className="shrink-0 flex items-center justify-center px-4 py-4">
-        <div className="w-full flex flex-col gap-4">
-          <div className="flex flex-row w-full gap-2 items-center justify-between">
-            <div className="flex-1 min-w-0 flex flex-col gap-2">
-              <p className="font-medium text-sm line-clamp-1">
-                {currentSong.title}
-              </p>
-              <p className="text-xs text-muted-foreground line-clamp-1">
-                {currentSong.artist}
-              </p>
+        <div className="flex items-center justify-center px-4 py-4">
+          <div className="w-full flex flex-col gap-4">
+            <div className="flex flex-row w-full gap-2 items-center justify-between">
+              <div className="flex flex-col gap-2">
+                <p className="font-medium text-sm line-clamp-1 text-ellipsis">
+                  {currentSong.title}
+                </p>
+                <p className="text-xs text-muted-foreground line-clamp-1 text-ellipsis">
+                  {currentSong.artist}
+                </p>
+              </div>
+              <div className="h-10 w-10">
+                <Waves start={isPlaying} />
+              </div>
+              <div className="flex items-center justify-between gap-2">
+                <button
+                  onClick={onClose}
+                  className={`cursor-pointer p-2 rounded-lg bg-accent text-muted-foreground`}
+                >
+                  <ListMusic className="h-5 w-5" />
+                </button>
+                <div
+                  onClick={() => setShowMenu(true)}
+                  className={`group relative cursor-pointer p-2 rounded-lg bg-accent text-muted-foreground`}
+                >
+                  <CircleEllipsis className="w-5 h-5" />
+                  {showMenu && (
+                    <div className="absolute bottom-full right-1/2 -translate-y-2">
+                      <SleeperTimer onClose={() => setShowMenu(false)} />
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className="flex items-center justify-between gap-2">
-              <button
-                onClick={() => setFavourite(!favourite)}
-                className={`cursor-pointer p-2 rounded-lg bg-accent ${
-                  favourite ? "text-primary" : "text-muted-foreground"
-                }`}
-              >
-                <Heart className="w-5 h-5" />
-              </button>
-              <div
-                onClick={() => setShowMenu(true)}
-                className={`group relative cursor-pointer p-2 rounded-lg bg-accent text-muted-foreground`}
-              >
-                <CircleEllipsis className="w-5 h-5" />
-                {showMenu && (
-                  <div className="absolute bottom-full right-1/2 -translate-y-2">
-                    <SleeperTimer onClose={() => setShowMenu(false)} />
+            <div className="flex flex-col gap-1">
+              <ProgressBar
+                progress={progress}
+                duration={duration}
+                onSeek={seek}
+              />
+              <div className="flex flex-row justify-between items-center gap-2">
+                <span className="text-xs text-muted-foreground w-10 text-right">
+                  {formatTime(progress)}
+                </span>
+                <span className="text-xs text-muted-foreground w-10">
+                  {formatTime(duration)}
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col justify-center">
+              <div className="w-full max-w-sm mx-auto space-y-2 px-4">
+                <div className="grid grid-cols-4 justify-items-center sm:gap-2 gap-8">
+                  <div className=" col-span-1">
+                    <button
+                      onClick={() => seek(progress - 15)}
+                      className="cursor-pointer p-2 rounded-full hover:bg-accent relative flex items-center justify-center"
+                    >
+                      <RotateCcw className="h-7 w-7 text-muted-foreground font-thin stroke-1" />
+                      <span className="absolute leading-none text-muted-foreground text-xs top-1/2 -translate-y-1/2">
+                        15
+                      </span>
+                    </button>
                   </div>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-1">
-            <ProgressBar
-              progress={progress}
-              duration={duration}
-              onSeek={seek}
-            />
-            <div className="flex flex-row justify-between items-center gap-2">
-              <span className="text-xs text-muted-foreground w-10 text-right">
-                {formatTime(progress)}
-              </span>
-              <span className="text-xs text-muted-foreground w-10">
-                {formatTime(duration)}
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <div className="w-full max-w-sm mx-auto space-y-2 px-4">
-              <div className="grid grid-cols-4 gap-8">
-                <div className="col-span-1">
-                  <button
-                    onClick={() => seek(progress - 15)}
-                    className="cursor-pointer p-2 rounded-full hover:bg-accent relative flex items-center justify-center"
-                  >
-                    <RotateCcw className="h-7 w-7 text-muted-foreground font-thin stroke-1" />
-                    <span className="absolute leading-none text-muted-foreground text-xs top-1/2 -translate-y-1/2">
-                      15
-                    </span>
-                  </button>
+                  <div className="col-span-2 w-full flex flex-row items-center justify-center gap-1">
+                    <button
+                      onClick={previous}
+                      className="p-2 cursor-pointer rounded-full hover:bg-accent"
+                    >
+                      <SkipBack className="h-6 w-6" />
+                    </button>
+                    <button
+                      onClick={togglePlay}
+                      className="rounded-full cursor-pointer bg-muted p-3"
+                    >
+                      {isPlaying ? (
+                        <Pause className="h-6 w-6" />
+                      ) : (
+                        <Play className="h-6 w-6" />
+                      )}
+                    </button>
+                    <button
+                      onClick={next}
+                      className="p-2 cursor-pointer rounded-full hover:bg-accent"
+                    >
+                      <SkipForward className="h-6 w-6" />
+                    </button>
+                  </div>
+                  <div className=" col-span-1">
+                    <button
+                      onClick={() => seek(progress + 15)}
+                      className="cursor-pointer p-2 rounded-full hover:bg-accent relative flex items-center justify-center"
+                    >
+                      <RotateCw className="h-7 w-7 text-muted-foreground font-thin stroke-1" />
+                      <span className="absolute leading-none text-muted-foreground text-xs top-1/2 -translate-y-1/2">
+                        15
+                      </span>
+                    </button>
+                  </div>
                 </div>
-                <div className="col-span-2 flex flex-row items-center justify-between gap-1">
-                  <button
-                    onClick={previous}
-                    className="p-2 cursor-pointer rounded-full hover:bg-accent"
-                  >
-                    <SkipBack className="h-6 w-6" />
-                  </button>
-                  <button
-                    onClick={togglePlay}
-                    className="rounded-full cursor-pointer bg-muted p-3"
-                  >
-                    {isPlaying ? (
-                      <Pause className="h-6 w-6" />
-                    ) : (
-                      <Play className="h-6 w-6" />
-                    )}
-                  </button>
-                  <button
-                    onClick={next}
-                    className="p-2 cursor-pointer rounded-full hover:bg-accent"
-                  >
-                    <SkipForward className="h-6 w-6" />
-                  </button>
-                </div>
-                <div className="col-span-1">
-                  <button
-                    onClick={() => seek(progress + 15)}
-                    className="cursor-pointer p-2 rounded-full hover:bg-accent relative flex items-center justify-center"
-                  >
-                    <RotateCw className="h-7 w-7 text-muted-foreground font-thin stroke-1" />
-                    <span className="absolute leading-none text-muted-foreground text-xs top-1/2 -translate-y-1/2">
-                      15
-                    </span>
-                  </button>
-                </div>
-              </div>
-              <div className="grid grid-cols-4 gap-8">
-                <div className="col-span-1">
-                  <button
-                    onClick={toggleShuffle}
-                    className={`p-2 cursor-pointer rounded-full hover:bg-accent ${
-                      shuffle ? "text-primary" : "text-muted-foreground"
-                    }`}
-                  >
-                    <Shuffle className="h-6 w-6" />
-                  </button>
-                </div>
-                <div className="col-span-2 flex justify-center">
-                  <VolumeSlider volume={volume} onVolumeChange={setVolume} />
-                </div>
-                <div className="col-span-1">
-                  <button
-                    onClick={toggleRepeat}
-                    className={`p-2 cursor-pointer rounded-full hover:bg-accent ${
-                      repeatMode !== "off"
-                        ? "text-primary"
-                        : "text-muted-foreground"
-                    }`}
-                  >
-                    <RepeatIcon className="h-6 w-6" />
-                  </button>
+                <div className="grid grid-cols-4 justify-items-center sm:gap-2 gap-8">
+                  <div className="col-span-1">
+                    <button
+                      onClick={toggleShuffle}
+                      className={`p-2 cursor-pointer rounded-full hover:bg-accent ${
+                        shuffle ? "text-primary" : "text-muted-foreground"
+                      }`}
+                    >
+                      <Shuffle className="h-6 w-6" />
+                    </button>
+                  </div>
+                  <div className="col-span-2 flex w-full justify-center">
+                    <VolumeSlider volume={volume} onVolumeChange={setVolume} />
+                  </div>
+                  <div className="col-span-1">
+                    <button
+                      onClick={toggleRepeat}
+                      className={`p-2 cursor-pointer rounded-full hover:bg-accent ${
+                        repeatMode !== "off"
+                          ? "text-primary"
+                          : "text-muted-foreground"
+                      }`}
+                    >
+                      <RepeatIcon className="h-6 w-6" />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
