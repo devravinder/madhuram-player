f6f41486b36383a1aa6964867531d87b24bfec6b
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-01-11 13:18:57 +0530

    feat: use hashrouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,22 +1,29 @@
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import {
+  createRouter,
+  RouterProvider,
+  createHashHistory,
+} from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const history = createHashHistory();
 
 // Set up a Router instance
 const router = createRouter({
   routeTree,
-  defaultPreload: 'intent',
+  defaultPreload: "intent",
   scrollRestoration: true,
-})
+  history,
+});
 
 // Register things for typesafety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 export type RoutePath = keyof typeof router.routesByPath;
 
 export default function AppRouter() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
