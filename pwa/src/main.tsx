46d454e8470f327906c935f2926f43e13b4f895e
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-02-11 17:43:14 +0530

    fix: env issue

--- pwa/src/main.tsx
+++ pwa/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { onAppStart } from "./services/initializer.ts";
-
 const intialize = async () => {
   await onAppStart();
 };
